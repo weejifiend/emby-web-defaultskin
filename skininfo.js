ab19e2c2bcda8c67b29870a5d36126ebef2b576a
weejifiend <weeji1@yahoo.com>
2017-01-26 15:41:14 -0500

    Update skininfo.js

--- skininfo.js
+++ skininfo.js
@@ -2,7 +2,7 @@ define([], function () {
     'use strict';
 
     return {
-        id: 'defaultskin',
-        name: 'Default Skin'
+        id: 'defaultskinweeji',
+        name: 'Default Skin (Weeji)'
     };
 });
